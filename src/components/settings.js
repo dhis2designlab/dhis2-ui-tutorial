fb2662a6d60e11009d10bc368ef412ea617b22dd
hannaevensen2405@gmail.com <hannaevensen2405@gmail.com>
2020-10-05 09:29:00 +0200

    #4 add icon for account

--- src/components/settings.js
+++ src/components/settings.js
@@ -5,6 +5,7 @@ import Grid from  '@material-ui/core/Grid';
 import Paper from  '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import { Button } from '@dhis2/ui-core'
+import { Account } from '@dhis2/ui-icons'
 
 const useStyles = makeStyles((theme) => ({
   cardGrid: {
@@ -24,20 +25,21 @@ const useStyles = makeStyles((theme) => ({
 
 function Settings({ onClick, user }) {
   const classes = useStyles();
+  console.log(Account)
 
   return (
     <>
       <HeaderBar user={user}/>
       <div className={classes.root}>
-        <Grid container spacing={3}>
+        <Grid container spacing={6}>
           <Grid item xs={12}>
             <div className={classes.paper}><h1>Settings</h1></div>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <div className={classes.paper}><p>insert image</p></div>
+            <div className={classes.paper}><Account /></div>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <div className={classes.paper}><p>Name</p></div>
+            <div className={classes.paper}><p>Firstname Lastname</p></div>
           </Grid>
           <Grid item xs={6} sm={3}>
             <div className={classes.paper}>Badge 1</div>
