ee323a4a11f31ce40f5ee12af832f6276e577313
hannaevensen2405@gmail.com <hannaevensen2405@gmail.com>
2020-10-06 20:40:42 +0200

    add extra information to each link

--- src/components/links.js
+++ src/components/links.js
@@ -2,26 +2,66 @@ import React from 'react';
 
 import Grid from '@material-ui/core/Grid';
 
+import { makeStyles } from '@material-ui/core/styles';
+
+import { Button } from '@dhis2/ui';
+
 import storybook from "../images/sb.png";
 
 import github from "../images/github_logo.png";
 
+const useStyles = makeStyles((theme) => ({
+    link: {
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto' 
+    },
+    header: {
+        textAlign: 'center'
+    }
+  
+  }));
 
 function Links() {
+const classes = useStyles();
   return (
     <Grid container spacing={4}>
-        <Grid item xs={12} sm={6} md={4}>
-            <img src={storybook} width="50%" alt="Storybook"></img>
-            <p>Link to storybook</p>
+        <Grid  item xs={12} sm={4} md={4}>
+            <img className={classes.link} src={storybook} width="30%" alt="Storybook"></img>
+            <h3 className={classes.header}>Storybook</h3>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam</p>
+            <Button
+                name="Basic button"
+                //onClick={() => setIsOpen(!isOpen)}
+                //onClick={handleChange}
+                type="button">
+                Go to Storybook
+            </Button>
         </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-            <img src={github} width="50%" alt="Github"></img>
-            <p>Link to Github</p>
+        <Grid item xs={12} sm={4} md={4}>
+            <img className={classes.link} src={github} width="30%" alt="Github"></img>
+            <h3 className={classes.header} >Github</h3>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium voluptatem accusantium doloremque laudantium</p>
+            <Button
+                name="Basic button"
+                //onClick={() => setIsOpen(!isOpen)}
+                //onClick={handleChange}
+                type="button">
+                Go to Github
+            </Button>
         </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-            <img src={storybook} width="50%" alt="storybook"></img>
-            <p>Link to blabla</p>
-    </Grid>
+        <Grid item xs={12} sm={4} md={4}>
+            <img className={classes.link} src={github} width="30%" alt="Github"></img>
+            <h3 className={classes.header}>Blabla</h3>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</p>
+            <Button
+                name="Basic button"
+                //onClick={() => setIsOpen(!isOpen)}
+                //onClick={handleChange}
+                type="button">
+                Go to X
+            </Button>
+        </Grid>
   </Grid>  
 
 )}
