7ea8ec2de17dc421aa51285b0d52e49ab4617eb7
hannaevensen2405@gmail.com <hannaevensen2405@gmail.com>
2020-11-03 11:00:58 +0100

    add modyle.css for parts

--- src/parts/NavBar/index.js
+++ src/parts/NavBar/index.js
@@ -5,60 +5,27 @@ import {
   Link,
 } from "react-router-dom";
 
-import { makeStyles } from '@material-ui/core/styles';
 import logo from "../../images/dhis2-logo.png";
 import Grid from '@material-ui/core/Grid';
 import { Settings } from '@dhis2/ui-icons';
 
+import styles from "./styles.module.css";
 
-const useStyles = makeStyles((theme) => ({
-  nav: {
-    background: 'white',
-    height: '64px',
-    position: 'fixed',
-    boxShadow: '0 2px 4px -1px rgba(0,0,0,0.25)',
-  },
-  icon: {
-    width: '40px',
-  },
-  settings: {
-    textAlign: 'right',
-    display: 'inline-block',
-    margin: 'auto',
-  },
-  name: {
-    color: 'black',
-    textAlign: 'right',
-    display: 'inline-block',
-  },
-  link: {
-    textDecoration: 'none',
-    textAlign: 'left',
-
-  },
-  img: {
-    margin: 'auto',
-  },
-  logo: {
-    maxHeight: '30px',
-  }
-
-}));
 
 
 function NavBar({user}) {
-  const classes = useStyles();
+
   const name = user ? user.username : ''
 
   return (
-    <Grid container  spacing={2} className={classes.nav}>
-      <Grid item className={classes.img} xs={5} sm={5} md={5}>
-        <img src={logo} className={classes.logo}/>
+    <Grid container  spacing={2} className={styles.nav}>
+      <Grid  className={styles.img} xs={5} sm={5} md={5}>
+        <img src={logo} className={styles.logo}/>
       </Grid>
-      <Grid item className={classes.settings} xs={5} sm={5} md={5}>
-        <Link className={classes.link} to={`/settings`}>
-          <p className={classes.name}>{name}</p>
-          <Settings className={classes.icon}/>  
+      <Grid className={styles.settings} xs={5} sm={5} md={5}>
+        <Link className={styles.link} to={`/settings`}>
+          <p className={styles.name}>{name}</p>
+          <Settings className={styles.icon}/>  
         </Link>
         </Grid>
     </Grid>
--- src/parts/Resources/index.js
+++ src/parts/Resources/index.js
@@ -2,28 +2,19 @@ import React from 'react';
 
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
-import { makeStyles } from '@material-ui/core/styles';
 
 import Link from '../../components/Link.js'
 
-const useStyles = makeStyles((theme) => ({
-    cardMedia: {
-        paddingTop: '56.25%', // 16:9
-      },
-    header: {
-        textAlign: 'center',
-    },
-  }));
+import styles from "./styles.module.css";
+
 
 function Links() {
 
-const classes = useStyles();
-
   return (
-    <div className={classes.cont}> 
+    <div> 
         <Container>
-            <Grid className={classes.container} container spacing={5}>
-                <Grid item xs={12} sm={12} md={12} className={classes.header}>
+            <Grid  container spacing={5}>
+                <Grid item xs={12} sm={12} md={12} className={styles.header}>
                     <h2>Resources</h2>
                 </Grid>
                 <Link />
