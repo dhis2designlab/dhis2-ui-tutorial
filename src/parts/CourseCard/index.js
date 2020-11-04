1fe0f98e9ebe0ef13fa7da62b8879cd3bdce79f9
hannaevensen2405@gmail.com <hannaevensen2405@gmail.com>
2020-11-04 13:52:02 +0100

    add hover effect on cards

--- src/parts/CourseCard/index.js
+++ src/parts/CourseCard/index.js
@@ -6,27 +6,15 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 
-
-import { makeStyles } from '@material-ui/core/styles';
-
 import { Button } from '@dhis2/ui';
 import {
   Link,
 } from "react-router-dom";    
 
+import styles from "./styles.module.css"
 
-const useStyles = makeStyles((theme) => ({
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
-  },
-
-  link: {
-    textDecoration: 'none',
-  }
-}));
 
 function CourseCard({section, index}) {
-  const classes = useStyles();
 
   const [isOpen, setIsOpen] = useState(false);
 
@@ -34,17 +22,17 @@ function CourseCard({section, index}) {
 
 
   return (
-    <Link className={classes.link} key={index}
+    <Link className={styles.link} key={index}
     to={{
       pathname: `/course/${index}`,
     }}> 
-      <Card className={classes.card}>
+      <Card className={styles.card}>
         <CardMedia
-          className={classes.cardMedia}
+          className={styles.cardMedia}
           image={quizId}
           title="Image title"
         />
-        <CardContent className={classes.cardContent}>
+        <CardContent className={styles.cardContent}>
           <h3>{title}</h3>
           <p>{short_info}</p>
         </CardContent>
